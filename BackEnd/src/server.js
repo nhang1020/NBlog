6fab7b36b94cc7c0a510826a4ce9cdc0c75aea94
TRAN THANH NHANG <94044017+nhang1020@users.noreply.github.com>
2023-08-16 16:24:57 +0700

    Update server.js

--- BackEnd/src/server.js
+++ BackEnd/src/server.js
@@ -8,15 +8,10 @@ require('dotenv').config();
 
 let app = express();
 app.use(function (req, res, next) {
-
     res.setHeader('Access-Control-Allow-Origin', process.env.URL_REACT);
-
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
     res.setHeader('Access-Control-Allow-Credentials', true);
-
     next();
 });
 
@@ -29,5 +24,5 @@ connectDB(app);
 
 let port = process.env.PORT || 8000;
 app.listen(port, () => {
-    console.log("NodeJS is runing at port: " + port);
+    console.log("Server is runing at port: " + port);
 })
